3842f064328452d04f25e5021e205ca05b27e312
amirkairzhan <amirkairzhan@bk.ru>
2024-11-02 14:30:54 +0700

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,4 +1,20 @@
 const audio = document.querySelector('.audio');
+const button = document.querySelector('.audio__btn');
+let isDefault = true;
+
+button.addEventListener("click", function() {
+    toggleBackImage();
+    toggleAudio();
+});
+
+function toggleBackImage() {
+    if (isDefault) {
+        button.style.backgroundImage = "url(resources/audio-btn-pause.jfif)";
+    } else {
+        button.style.backgroundImage = "url(/resources/audio-btn.png)";
+    }
+    isDefault = !isDefault;
+}
 
 function toggleAudio() {
     if (audio.paused) {
